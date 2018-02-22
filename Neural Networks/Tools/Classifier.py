9ee458dad99ef2afe220270f56777ef9bb661b0f
Sounak Banerjee <sounakban.science@example.com>
2018-02-22 21:24:35 +0530

    Sync

--- Neural Networks/Tools/Classifier.py
+++ Neural Networks/Tools/Classifier.py
@@ -51,9 +51,9 @@ class CNN_Classifier:
         model_conv = Concatenate()(conv_blocks) if len(conv_blocks) > 1 else conv_blocks[0]
 
         model_hidden = Dropout(0.3)(model_conv)
-        model_hidden = Dense(1024, activation="relu")(model_hidden)
+        model_hidden = Dense(2024, activation="relu")(model_hidden)
         model_hidden = Dropout(0.5)(model_hidden)
-        model_hidden = Dense(256, activation="relu")(model_hidden)
+        model_hidden = Dense(512, activation="relu")(model_hidden)
         model_hidden = Dropout(0.2)(model_hidden)
         model_hidden = Dense(64, activation="relu")(model_hidden)
         model_output = Dense(class_count, activation="softmax")(model_hidden)
