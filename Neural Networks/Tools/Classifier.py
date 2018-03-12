5d924e672f2cc55fda57019fd379305b28897f01
Sounak Banerjee <sounakban.science@example.com>
2018-03-12 21:28:40 +0530

    Sync

--- Neural Networks/Tools/Classifier.py
+++ Neural Networks/Tools/Classifier.py
@@ -192,6 +192,66 @@ class CNN_Classifier:
 
 
 
+class BDRNN_Classifier:
+
+    def __init__(self, output_size, learning_rate=0.001, batch_size=64, num_epochs=10):
+        self.output_size = output_size
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.learning_rate = learning_rate
+        print("Using RNN with {} Neurons : ", self.output_size)
+        print("Using RNN with parameters : \nBatch-size : {},  \
+                                            \nLearning-Rate : {}".format \
+                                            (self.batch_size, self.learning_rate) )
+
+
+    def predict(self, x_train, y_train, x_test, y_test, embeddings, sequence_length, class_count):
+        req_type = type(np.array([]))
+        assert type(x_train) == req_type and type(x_test) == req_type
+        assert type(y_train) == req_type and type(y_test) == req_type
+
+        from keras.models import Model
+        from keras.layers import Input, Dense, Dropout, Flatten, Embedding, LSTM, Bidirectional
+        from keras.layers.merge import Concatenate
+        from keras.optimizers import Adam, Adagrad
+
+        input_shape = (sequence_length,)
+        model_input = Input(shape=input_shape)
+        print("Input tensor shape: ", model_input.get_shape)
+        # model_embedding = Embedding(embeddings.shape[0], embeddings.shape[1], input_length=sequence_length, name="embedding")(model_input)
+        model_embedding = Embedding(embeddings.shape[0], embeddings.shape[1], weights=[embeddings], name="embedding")(model_input)
+        print("Embeddings tensor shape: ", model_embedding.get_shape)
+        # model_embedding = Dropout(0.4)(model_embedding)
+        # model_recurrent  = LSTM(int(embeddings.shape[1]*1.5), activation='tanh', dropout=0.2, recurrent_dropout=0.2)(model_embedding)
+        # model_recurrent  = LSTM(embeddings.shape[1], activation='tanh', dropout=0.2)(model_embedding)
+        model_recurrent = Bidirectional(LSTM(embeddings.shape[1], activation='tanh', dropout=0.2))(model_embedding)
+
+        # model_hidden = Dense(1536, activation="relu")(model_recurrent)
+        # model_hidden = Dropout(0.7)(model_hidden)
+        model_hidden = Dense(512, activation="relu")(model_recurrent)
+        model_hidden = Dropout(0.7)(model_hidden)
+        model_hidden = Dense(64, activation="relu")(model_hidden)
+        model_output = Dense(class_count, activation="softmax")(model_hidden)
+        # model_output = Dense(1, activation="sigmoid")(model_hidden)
+
+        model = Model(model_input, model_output)
+        optimizer = Adam(lr=self.learning_rate)
+        # optimizer = Adagrad(lr=self.learning_rate)
+        model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+        # model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+        print(type(x_train), " ; ", type(y_train))
+        print(x_train.shape, ' ; ', x_test.shape)
+        # model.fit(x_train, y_train, batch_size=self.batch_size, epochs=self.num_epochs,
+        #   validation_data=(x_test, y_test), verbose=2, shuffle=True)
+
+        model.fit(x_train, y_train, batch_size=self.batch_size, epochs=self.num_epochs,
+          validation_split=0.2, verbose=2, shuffle=True)
+
+        test_model(model, x_test, y_test)
+
+        return 0
+
 
 
 class RNN_Classifier:
